2af23f67d9876a8b7e6563b986abba2a66744dd1
Evgeny Davydov <jerryngtond@gmail.com>
2019-02-10 13:28:17 +0300

    HW-1
    1) Реализация методов по работе с storage
    2) Тестирование методов

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,58 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+    private int size;
+
+    ArrayStorage() {
+        this.size = 0;
+    }
+
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < this.size(); i++) {
+            this.storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        if (this.size() < this.storage.length) {
+            this.storage[this.size()] = r;
+            this.size++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (this.storage[i].toString().equals(uuid)) {
+                return this.storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < this.size(); i++) {
+            if (this.storage[i].toString().equals(uuid)) {
+                for (int j = i; j < this.size(); j++) {
+                    this.storage[j] = this.storage[j + 1];
+                }
+                this.size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(this.storage, 0, this.size());
     }
 
     int size() {
-        return 0;
+        return this.size;
     }
 }
