338f61f57463756c0b9990f89424f241498b3e45
jerryngton <jerryngtond@gmail.com>
2019-03-11 06:33:53 +0300

    Lesson-4
    1) Implementation of basic exceptions

---
+++ src/com/jbrise/webapp/exception/ExistStorageException.java
@@ -0,0 +1,7 @@
+package com.jbrise.webapp.exception;
+
+public class ExistStorageException extends StorageException {
+    public ExistStorageException(String uuid) {
+        super("Resume " + uuid + " already exist", uuid);
+    }
+}
