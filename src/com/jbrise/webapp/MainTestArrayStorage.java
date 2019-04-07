021dd33ed1a7b8dc9e833f9d7917f69e30512d32
Evgeny Davydov <jerryngtond@gmail.com>
2019-04-07 14:31:19 +0300

    test fixed

--- src/com/jbrise/webapp/MainTestArrayStorage.java
+++ src/com/jbrise/webapp/MainTestArrayStorage.java
@@ -3,6 +3,9 @@ package com.jbrise.webapp;
 import com.jbrise.webapp.model.Resume;
 import com.jbrise.webapp.storage.ArrayStorage;
 
+import com.jbrise.webapp.exception.NotExistStorageException;
+
+
 /**
  * Test for your ArrayStorage implementation
  */
@@ -13,7 +16,7 @@ public class MainTestArrayStorage {
         final Resume r1 = new Resume();
         final Resume r2 = new Resume();
         final Resume r3 = new Resume();
-        Resume updatableResume = new Resume();
+        Resume updatableResume = new Resume(r3.getUuid());
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
@@ -28,7 +31,11 @@ public class MainTestArrayStorage {
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        try {
+            System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        } catch (NotExistStorageException exception) {
+            System.out.println("Expected NotExistStorageException so it's okey, cause dummy doesn't exist in storage");
+        }
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
