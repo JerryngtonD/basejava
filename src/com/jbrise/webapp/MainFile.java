36d8282dcf9cad81543edbfd17840d9282a58f92
jerryngton <jerryngtond@gmail.com>
2019-06-06 13:38:27 +0300

    work with files and resources and in russian - pizdec if you do it with previous handling exceptions

---
+++ src/com/jbrise/webapp/MainFile.java
@@ -0,0 +1,51 @@
+package com.jbrise.webapp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) {
+        String filePath = "./.gitignore";
+        File file = new File(filePath);
+        System.out.println(file.exists());
+        System.out.println("/--------------------------/\n");
+
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+        System.out.println("/--------------------------/\n");
+
+
+        File dir = new File("./src/com/jbrise/webapp");
+        System.out.println(dir.isDirectory());
+        System.out.println("/--------------------------/\n");
+
+        String[] fileEntities = dir.list();
+        if(fileEntities != null) {
+            for(String name : fileEntities) {
+                System.out.println(name);
+            }
+        }
+
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+            System.out.println(fileInputStream.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
